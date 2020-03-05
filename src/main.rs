ed6af05b1f5d983047393831b23f52725b10cca9
Miklos Maroti <mmaroti@gmail.com>
2020-03-05 15:31:12 +0100

    not much

--- src/main.rs
+++ src/main.rs
@@ -113,11 +113,16 @@ pub fn test_solver2(solver_name: &str, size: usize) -> String {
     let rel = sol.add_variable(Shape::new(vec![size, size]));
 
     let rfl = sol.polymer(&rel, Shape::new(vec![size]), &[0, 0]);
+    let rfl = sol.tensor_all(&rfl);
     sol.add_clause(&[&rfl]);
 
     let inv = sol.polymer(&rel, Shape::new(vec![size, size]), &[1, 0]);
-    let neg = sol.tensor_not(&rel);
-    sol.add_clause(&[&neg, &inv]);
+    //let neg = sol.tensor_not(&rel);
+    //sol.add_clause(&[&neg, &inv]);
+    let imp = sol.tensor_leq(&rel, &inv);
+    let imp = sol.tensor_all(&imp);
+    let imp = sol.tensor_all(&imp);
+    sol.add_clause(&[&imp]);
 
     let r01 = sol.polymer(&rel, Shape::new(vec![size, size, size]), &[0, 1]);
     let r01 = sol.tensor_not(&r01);
